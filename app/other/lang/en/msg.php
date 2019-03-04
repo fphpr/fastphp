97cf1370f54d84407a4fdc35694b1e818ea89a4c
fphpr <fastphpframework@gmail.com>
2019-03-04 22:08:38 +0330

    clean

--- app/other/lang/en/msg.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$lang_array=[
-  'error_net'=>'Check your network connection and try again',
-];
--- public/js/component.js
+++
@@ -1,6 +0,0 @@
-
-  function getJson(str){
-    str = str.replace(/\n/g, '\\n');
-    str=str.replace(/\r/g, '\\r');
-    return JSON.parse(str);
-  }
