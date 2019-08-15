e0b34826c567084ba54fb63d1264c010623a1781
fphpr <fastphpframework@gmail.com>
2019-08-15 19:23:44 +0430

    add ~ 30 function to db table

--- app/Lib/Web.php
+++ app/Lib/Web.php
@@ -203,7 +203,7 @@ class File
     header("Pragma: no-cache");  header("Content-type: application/file");
     header('Content-length: '.filesize($file_to_download));
     header('Content-disposition: attachment; filename='.basename($file_to_download));
-    readfile($file_to_download);
+    getContent($file_to_download);
     exit;
   }
 
@@ -278,7 +278,7 @@ class File
     return $files;
   }
 
-  public static function readFile($name)
+  public static function getContent($name)
   {
     $text='';
     $myfile = fopen($name, "r");;
@@ -438,6 +438,14 @@ class DB
     return DB::initOnceDB(DB::getFirstConfigKey());
   }
 
+  public function table($name)
+  {
+    $db= DB::mainDB();
+
+    $table= new queryBuilder($name,$db);
+    return $table;
+  }
+
 }
 
 class query{
@@ -562,6 +570,369 @@ class query{
     $command = "mysqldump --opt -h$dbhost -u$dbuser -p$dbpass $dbname > $filename";
     system($command);
     return ['ok'=>true,'file_name'=>$filename];
+  }
+
+}
+
+class queryBuilder{
+  public $arr=[];
+  public $arr_values=[];
+  public $values_count=0;
+
+  public $tb_name;
+  public $db;
+
+  function __construct($tb_name,$db)
+  {
+    $this->tb_name=$tb_name;
+    $this->db=$db;
+  }
+
+
+
+  public function getTableName()
+  {
+    return '`'.$this->tb_name.'`';
+  }
+
+
+  public function getSql()
+  {
+    $arr=$this->arr;
+    ksort($arr);
+
+    foreach ($arr as $key => $value) {
+      $str.=$value;
+    }
+
+    return $str;
+  }
+
+
+  //==================================
+ //=======/ SELECT Functions \=======
+ //==================================
+
+
+ private function selectInit($select=true,$all=false,$from=false,$where=false)
+ {
+
+   if ($select) {
+     $index=$this->SelectSyntaxIndex('SELECT');
+     if ($this->arr[$index]==null) {
+       $this->arr[$index]="SELECT ";
+     }
+   }
+
+   if ($all) {
+     $index_all=$this->SelectSyntaxIndex('ALL');
+     if ($this->arr[$index_all] ==null) {
+       $this->arr[$index_all]=" * ";
+     }
+   }
+
+   if ($from) {
+     $index_from=$this->SelectSyntaxIndex('FROM');
+     if ($this->arr[$index_from] ==null) {
+       $this->arr[$index_from]=" FROM ".$this->getTableName().' ';
+     }
+   }
+
+   if ($where) {
+     $index_where=$this->SelectSyntaxIndex('WHERE');
+     if ($this->arr[$index_where] ==null) {
+       $this->arr[$index_where]=" WHERE ";
+     }
+   }
+
+ }
+
+  public function select($params=null)
+  {
+    $index=$this->SelectSyntaxIndex('ALL');
+
+    if (is_string($params)) {
+      $this->arr[$index]=" $params ";
+    }
+    elseif (is_array($params)) {
+
+      foreach($params as $key=> $field){
+        $pos=strpos($field,' ');
+
+        if ($pos > -1 ) {
+          $temp=substr($field,0,$pos);
+
+          $field=substr($field,$pos);
+          $temp="`$temp`";
+
+          $temp.=$field;
+          $params[$key]=$temp;
+        }
+        else {
+          $params[$key]="`$field`";
+        }
+      }
+      $params=implode(',',$params);
+      $this->arr[$index]=" $params ";
+    }
+
+    return $this;
+  }
+
+  public function where($name,$val1,$val2=null,$fl="AND")
+  {
+
+    $value='';
+    $op='=';
+    if ($val2!=null) {
+      $op=$val1;
+      $value=$val2;
+    }
+    else {
+      $value=$val1;
+    }
+
+    $key=$this->getKeyAndSetValue($name,$value);
+
+    $this->whereStr("`$name` $op $key ",$fl);
+    return $this;
+  }
+
+  public function orWhere($name,$val1,$val2=null)
+  {
+    $this->where($name,$val1,$val2,"OR");
+    return $this;
+  }
+
+  public function whereIn($name,array $params,$fl='AND',$in="IN")
+  {
+    $index=$this->SelectSyntaxIndex('WHERE_STR');
+
+    $keys=[];
+
+    foreach ($params as $k => $val) {
+      $key=$this->getKeyAndSetValue($name,$val);
+      $keys[]=$key;
+    }
+
+    $this->whereStr("`$name` $in( ".implode(',',$keys)." ) ",$fl);
+    return $this;
+  }
+
+  public function whereNotIn($name,$val1)
+  {
+    $this->whereIn($name,$val1,'AND',"NOT IN");
+    return $this;
+  }
+
+  public function orWhereIn($name,$val1)
+  {
+    $this->whereIn($name,$val1,'OR');
+    return $this;
+  }
+  public function orWhereNotIn($name,$val1)
+  {
+    $this->whereIn($name,$val1,'OR','NOT IN');
+    return $this;
+  }
+
+  public function whereBetween($name,$val1,$val2,$fl='AND')
+  {
+    $between1=$this->getKeyAndSetValue($name,$val1);
+    $between2=$this->getKeyAndSetValue($name,$val2);
+
+    $this->whereStr(" `$name` BETWEEN $between1 AND $between2 ",$fl);
+    return $this;
+  }
+
+  public function orWhereBetween($name,$val1,$val2)
+  {
+    $this->whereBetween($name,$val1,$val2,'OR');
+    return $this;
+  }
+
+  //###################[null]####################
+
+  public function whereNull($name,$fl='AND')
+  {
+    $this->whereStr("`$name` IS NULL ",$fl);
+    return $this;
+  }
+
+  public function whereNotNull($name,$fl='AND')
+  {
+    $this->whereStr("`$name` IS NOT NULL ",$fl);
+    return $this;
+  }
+
+  public function orWhereNull($name)
+  {
+    $this->whereNull($name,'OR');
+    return $this;
+  }
+
+  public function orWhereNotNull($name)
+  {
+    $this->whereNotNull($name,'OR');
+    return $this;
+  }
+
+  //###################[dateTime]####################
+  public function whereDate($name,$date)
+  {
+    $this->whereBetween($name,"$date 00:00:00","$date 23:59:59");
+    return $this;
+  }
+
+  public function whereMonth($name,$month,$fl='AND')
+  {
+    $key=$this->getKeyAndSetValue($name,$month);
+    $this->whereStr(" MONTH(`$name`) = $key ",$fl);
+    return $this;
+  }
+
+  public function whereDay($name,$day,$fl='AND')
+  {
+    $key=$this->getKeyAndSetValue($name,$day);
+    $this->whereStr(" DAY(`$name`) = $key ",$fl);
+    return $this;
+  }
+
+  public function whereYear($name,$year,$fl='AND')
+  {
+    $key=$this->getKeyAndSetValue($name,$year);
+    $this->whereStr(" YEAR(`$name`) = $key ",$fl);
+    return $this;
+  }
+
+  public function whereTime($name,$time,$fl='AND')
+  {
+    $key=$this->getKeyAndSetValue($name,$time);
+    $this->whereStr(" TIME(`$name`) = $key ",$fl);
+    return $this;
+  }
+
+  public function whereColumn($val1,$val2,$val3=null,$fl='AND')
+  {
+    $value1='';
+    $value2='';
+
+    $op='=';
+    if ($val3!=null) {
+      $op=$val2;
+      $value1=$val1;
+      $value2=$val3;
+    }
+    else {
+      $value1=$val1;
+      $value2=$val2;
+    }
+
+    $this->whereStr(" `$value1` $op `$value2` ",$fl);
+    return $this;
+  }
+
+  public function orWhereColumn()
+  {
+    $this->whereColumn($val1,$val2,$val3,'OR');
+    return $this;
+  }
+
+
+  private function whereStr($query,$fl="AND")
+  {
+    $this->selectInit(false,false,false,true);
+    $index=$this->SelectSyntaxIndex('WHERE_STR');
+
+    if ($this->arr[$index]!=null) {
+      $this->arr[$index].=" $fl ";
+    }
+
+    $this->arr[$index].=$query;
+  }
+
+
+
+  //==================================
+ //=======/ ORDER Functions \========
+ //==================================
+
+  public function orderBy($name,$order='ASC')
+  {
+    $index=$this->SelectSyntaxIndex('ORDER_BY');
+    $this->arr[$index]=" ORDER BY $name $order ";
+    return $this;
+  }
+
+   //==================================
+  //=======/ JOIN Functions \=========
+  //==================================
+
+  public function join($name,$rel_id,$joinType='INNER')
+  {
+    $index=$this->SelectSyntaxIndex('JOIN');
+    $this->arr[$index].=" $joinType JOIN $name ON $rel_id ";
+    return $this;
+  }
+
+  public function leftJoin($name,$rel_id){
+    $this->join($name,$rel_id,'LEFT');
+    return $this;
+  }
+
+  public function rightJoin($name,$rel_id){
+    $this->join($name,$rel_id,'LEFT');
+    return $this;
+  }
+
+  public function fullJoin($name,$rel_id){
+    $this->join($name,$rel_id,'FULL OUTER');
+    return $this;
+  }
+
+
+  private function getKeyAndSetValue($name,$value)
+  {
+    $this->values_count++;
+    $key=":$name".'_'.$this->values_count;
+    $this->arr_values[$key]=$value;
+    return $key;
+  }
+
+  public function get()
+  {
+    $this->selectInit(true,true,true);
+    $query= $this->getSql();
+
+
+    return $this->db->execute($query,$this->arr_values,true);
+  }
+
+  public function SelectSyntaxIndex($key=null)
+  {
+    $arr=[
+      'SELECT'=>1,
+      'FIELDS'=>3,
+      'ALL'=>4,
+      'DISTINCT '=>8,
+      'DISTINCTROW'=>10,
+      'HIGH_PRIORITY'=>15,
+      'STRAIGHT_JOIN'=>20,
+      'FROM'=>25,
+      'JOIN'=>28,
+      'WHERE'=>30,
+      'WHERE_STR'=>31,
+      'GROUP_BY'=>35,
+      'HAVING'=>40,
+      'ORDER_BY'=>45,
+      'LIMIT'=>50,
+    ];
+    if ($key==null) {
+      return $arr;
+    }
+    else {
+      return $arr[$key];
+    }
   }
 
 }
